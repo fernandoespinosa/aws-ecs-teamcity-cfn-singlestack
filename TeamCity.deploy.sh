049dba3d4d95073ca177f1042e23a65f297fc3fd
Fernando Espinosa <fernandoespinosa@fernandoespinosa.org>
2019-07-16 01:56:03 -0400

    fix: `TeamCityVersion=2018.2.4`

--- TeamCity.deploy.sh
+++ TeamCity.deploy.sh
@@ -5,13 +5,13 @@ aws cloudformation deploy \
     --stack-name=TeamCity \
     --template-file=TeamCity.template.yaml \
     --parameter-overrides \
-        TeamCityVersion=latest \
+        TeamCityVersion=2018.2.4 \
         InternetFacingStack=true \
         DBPassword=${TEAMCITY_DB_PASSWORD} \
         ServerContainerCpu=2048 \
-        AgentInstanceType=t3.large \
+        AgentInstanceType=t3.medium \
         AgentContainerCpu=2048 \
-        AgentContainerMemory=7500 \
+        AgentContainerMemory=3700 \
         AgentNumber=0 \
         CertificateArn=arn:aws:acm:us-east-1:879709723808:certificate/3bfd5aa8-b81b-4209-bab1-ba6ae13cf8ae \
         CertificateDomainName=teamcity.dev.fernandoespinosa.org \
